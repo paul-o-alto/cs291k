4e65be1b2e1bda1dc720ce778e694363d33c9563
paul-o-alto <galloway.paul.f@gmail.com>
2016-04-28 20:10:04 -0700

    Finally works

--- MP2/conv_net.py
+++ MP2/conv_net.py
@@ -5,14 +5,16 @@ import math
 from data_utils import *
 
 input_size = 32*32*3
-output_size = 20
-# SPECIFIED IN PLACEHOLDER INSTEAD
+batch_size  = 100
+num_classes = 20
 
 images_placeholder = tf.placeholder(tf.float32, 
-                                    shape=[32, 32, 3],#32*32],
+                                    shape=(batch_size, 32, 32, 3),
                                     name='images')
-labels_placeholder = tf.placeholder(tf.float32, 
-                                    shape=(20,), #[None, 100] 
+labels_placeholder = tf.placeholder(#tf.int32, 
+                                    tf.float32, 
+                                    shape=(batch_size,#),
+                                           num_classes), 
                                     name='labels')
 
 def conv2d(x, W):
@@ -27,17 +29,18 @@ def max_pool(x, k):
                           padding='SAME')
 
 
+
 # Organize 1st-layer
 with tf.name_scope('firstlayer-conv'): 
 
-    images_reshaped = tf.reshape(images_placeholder,
-                                 shape=[-1, 32, 32, 3])
+    #images_reshaped = tf.reshape(images_placeholder,
+    #                             shape=[-1, 32, 32, 3])
     W_convl = tf.Variable(# 5x5 pixel patch, depth 3, 4 feature maps
                   tf.truncated_normal(shape=[5,5,3,4], 
                                       stddev=1e-4))
     b_convl = tf.constant(0.1, shape=[4]) # 4 feature maps
-    h_convl = tf.nn.relu(conv2d(images_reshaped, 
-                                #images_placeholder,
+    h_convl = tf.nn.relu(conv2d(#images_reshaped, 
+                                images_placeholder,
                                 W_convl) + b_convl)
 
 
@@ -62,40 +65,46 @@ with tf.name_scope('fully-connected-hidden'):
 
     h_w = tf.Variable(
               tf.truncated_normal(
-                  [8*8*W_convl.get_shape().as_list()[-1], 1024],
+                  [8*8*W_convl3.get_shape().as_list()[-1], 1024],
                   stddev=1.0/math.sqrt(float(input_size))),
               name='hidden_weights')
 
-    hidden = tf.reshape(h_pool4, [-1, h_w.get_shape().as_list()[0]])
+    h_pool4 = tf.reshape(h_pool4, [-1, h_w.get_shape().as_list()[0]])
     h_b = tf.Variable(tf.zeros([1024]),
                       name='hidden_biases')
-    hidden = tf.nn.relu(tf.nn.bias_add(tf.matmul(#h_pool4,
-                                                 hidden, 
+    hidden = tf.nn.relu(tf.nn.bias_add(tf.matmul(h_pool4, 
                                                  h_w), h_b))
 
 with tf.name_scope('fully-connected-mlp'):
     
     p_w = tf.Variable(
               tf.truncated_normal( 
-                  [1024, output_size],
+                  [1024, num_classes],
                   stddev=1.0),           #/math.sqrt(float(8*8))), 
               name='output_weights')
-    p_b = tf.Variable(tf.zeros([output_size]),
+    p_b = tf.Variable(tf.zeros([num_classes]),
                       name='output_biases')
     logits = tf.add(tf.matmul(hidden, p_w), p_b)
-    logits = tf.transpose(logits)
-    #logits.set_shape((labels_placeholder.get_shape()[0],)) 
-    # ugly hack
 
-#print logits
-#print labels_placeholder
+
+# Utility function to convert batch labels to one hot encoding
+# matrix
+#with tf.name_scope('make_one_hot'):
+#    sparse_labels = tf.reshape(labels_placeholder, [-1, 1])
+#    num_labels  = tf.shape(sparse_labels)[0]
+#    indices  = tf.reshape(tf.range(0, num_labels, 1), [-1, 1])
+#    concated = tf.concat(1, [indices, sparse_labels])
+#    outshape = tf.pack([num_labels, num_classes])
+#    labels   = tf.sparse_to_dense(concated,
+#                                  outshape, 1.0, 0.0)
 
 # Correct loss for convnet?
 with tf.name_scope('loss'):
-    print logits
-    print labels_placeholder
+    # Need sparse_ version in order to not have to deal with the 
+    # one hot encoding ourselves (hopefully)
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-                              logits, # sparse?
+                              logits, 
+                              #labels, 
                               labels_placeholder, 
                               name='xentropy')
     loss = tf.reduce_mean(cross_entropy, name='xentropy_mean')
@@ -117,12 +126,13 @@ max_iters = 100000
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables()) 
 
-    summary_op = tf.merge_all_summaries() # Do these go here?
-    summary_writer = tf.train.SummaryWriter("./log_temp", #log_dir, 
-                                            graph_def=sess.graph_def)
+    #summary_op = tf.merge_all_summaries() # Do these go here?
+    #summary_writer = tf.train.SummaryWriter("./log_temp", #log_dir, 
+    #                                        graph_def=sess.graph_def)
 
     X_train, y_train, X_test, y_test = load_CIFAR100(
                                            './cifar-100-batches-py/')
+    
     num_train = 49000
     num_val   = 1000
     num_test  = 10000
@@ -137,21 +147,31 @@ with tf.Session() as sess:
     #X_test  = X_test.reshape(num_test,  -1)
 
 
+    # Utility function to convert batch labels to one hot encoding
+    # matrix (numpy)
+    def make_one_hot(labels_dense):
+        num_labels = labels_dense.shape[0]
+        index_offset = np.arange(num_labels) * num_classes
+        labels_one_hot = np.zeros((num_labels, num_classes))
+        labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+        return labels_one_hot
+
     step = 1
-    batch_size = 100
     while step*batch_size < max_iters:
 
         offset = (step*batch_size) % (y_train.shape[0] - batch_size)
-        batch_data   = X_train[offset:(offset + batch_size)]
+        batch_data   = X_train[offset:(offset + batch_size), :]
         batch_labels = y_train[offset:(offset + batch_size)]
-
+        batch_labels = make_one_hot(batch_labels)
+        print batch_labels
+  
         feed_dict = {images_placeholder:batch_data,
                      labels_placeholder:batch_labels}
 
         _, loss_value = sess.run([train_op, loss],feed_dict=feed_dict)
 
-        summary_str = sess.run(summary_op, feed_dict=feed_dict)
-        summary_writer.add_summary(summary_str, step)
+        #summary_str = sess.run(summary_op, feed_dict=feed_dict)
+        #summary_writer.add_summary(summary_str, step)
         print "Iteration "+str(step*batch_size)
         print "Loss= %d" % loss_value
         step += 1
